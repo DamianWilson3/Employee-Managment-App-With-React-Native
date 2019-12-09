0c0cbb519d9042700db59c23e9470a48c68ebfd2
Damian Wilson <wlsdam001@myuct.ac.za>
2019-12-09 11:56:48 +0200

    Added Firebase Database Delete functionality - fire employee

--- src/components/EmployeeEdit.js
+++ src/components/EmployeeEdit.js
@@ -2,11 +2,13 @@ import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import EmployeeForm from './EmployeeForm';
-import { employeeUpdate, employeeSave } from '../actions';
-import { Card, CardSection, Button } from './common';
+import { employeeUpdate, employeeSave, employeeDelete } from '../actions';
+import { Card, CardSection, Button, Confirm } from './common';
 import Communications from 'react-native-communications';
 
 class EmployeeEdit extends Component {
+    state = { showModal: false};
+
     componentDidMount() {
         _.each(this.props.employee, (value, prop) => {
             this.props.employeeUpdate({ prop, value });
@@ -24,6 +26,16 @@ class EmployeeEdit extends Component {
         const { phone, shift } = this.props;
 
         Communications.text(phone, `Your upcoming shift is on ${shift}`);
+    }
+
+    onAccept() {
+        const { uid } = this.props.employee;
+
+        this.props.employeeDelete({ uid });
+    }
+
+    onDecline() {
+        this.setState({ showModal: false });
     }
 
     render() {
@@ -44,6 +56,21 @@ class EmployeeEdit extends Component {
                     </Button>
 
                 </CardSection>
+
+                <CardSection>
+                    <Button onPress={() => this.setState({ showModal: !this.state.showModal })}>
+                        Fire Employee
+                    </Button>
+
+                </CardSection>
+
+                <Confirm
+                    visible={this.state.showModal}
+                    onAccept={this.onAccept.bind(this)}
+                    onDecline={this.onDecline.bind(this)}
+                >
+                    Are you sure you want to fire this person?
+                </Confirm>
             </Card>
         );
     }
@@ -55,4 +82,5 @@ const mapStateToProps = (state) => {
     return{ name, phone, shift };
 };
 
-export default connect(mapStateToProps, { employeeUpdate, employeeSave })(EmployeeEdit);
+export default connect(mapStateToProps, { employeeUpdate, employeeSave, employeeDelete })(EmployeeEdit);
+
